5352cc6c68f20022e79e4159d46ed440170d1aee
frolushka <mpfrolovstud@gmail.com>
2020-10-01 21:03:44 +0300

    add exercise templates

---
+++ Assets/ex03/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] private float gravity;
+    [SerializeField] private float punchPower;
+
+    private float _currentVerticalVelocity;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _currentVerticalVelocity = punchPower;
+        }
+
+        transform.position += Time.deltaTime * _currentVerticalVelocity * Vector3.up;
+        _currentVerticalVelocity -= Time.deltaTime * gravity;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log($"Score: TODO\nTime: {Mathf.RoundToInt(Time.time)}s");
+    }
+}
